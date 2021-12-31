07dc397030dec007495d89e05fa2c1b71d77f99b
이병훈 <bhlee@onns.co.kr>
2021-12-31 13:55:03 +0900

    최종 코드

--- UserDefaults_tutorial/ViewController.swift
+++ UserDefaults_tutorial/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var label: UILabel!
     
-    
-    
+    // 변수 처리
+    let defaults = UserDefaults.standard
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 값 불러오기
+        let getTextValue = defaults.string(forKey: "textValue")
+        
+        // getTextValue가 nil이면 label에 "기본값" 보여주기
+        label.text = getTextValue ?? "기본값"
         
     }
 
@@ -27,6 +33,9 @@ class ViewController: UIViewController {
         // textField에 적힌 글을 label에 보여주기
         label.text = textField.text
     
+        // textField에 있는 글 저장
+        defaults.set(textField.text, forKey: "textValue")
+
       
     }
     
